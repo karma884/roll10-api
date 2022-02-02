4b71461df3f1f2a91041101625f9e5831a672411
karma884 <ray.hirsh@gmail.com>
2022-02-02 15:11:19 +0200

    mongo works locally, not deployed on Heroku yet

--- index.js
+++ index.js
@@ -2,33 +2,53 @@ const express = require("express");
 const cors = require("cors");
 
 const app = express();
-let message = "Hi owl";
-let historicalRolls = [];
 
 app.use(express.json());
 app.use(cors());
 
-app.get("/entries", (req, res) => {
-  console.log("we got a request to see historicalRolls!");
-  res.set("Access-Control-Allow-Origin", "*");
+const MongoClient = require("mongodb").MongoClient;
+const url = "mongodb://localhost:27017/roll10db";
+let db;
 
-  res.json(historicalRolls);
+MongoClient.connect(url, function (err, database) {
+  if (err) throw err;
+  db = database.db("roll10db");
 });
 
-app.post("/entries", (req, res) => {
-  console.log("we got a new ENTRY");
-  //console.log(req.body);
-  entry = req.body;
-  historicalRolls = [entry, ...historicalRolls];
+app.get("/entries", async (req, res) => {
+  console.log("we got a request to see historicalRolls!");
 
-  res.json(historicalRolls);
+  let historicalRollsDb = await db
+    .collection("historicalRolls")
+    .find({})
+    .toArray();
+  res.json(historicalRollsDb);
+});
+
+app.post("/entries", async (req, res) => {
+  console.log("we got a new entry!");
+  entry = req.body;
+  const { insertedId } = await db
+    .collection("historicalRolls")
+    .insertOne(entry);
+
+  let insertedEntry = await db
+    .collection("historicalRolls")
+    .findOne({ _id: insertedId });
+  res.json(insertedEntry);
 });
 
 app.post("/newmessages", (req, res) => {
-  console.log("we got a COMMENT");
+  console.log("we got a request to edit a comment!");
   const { comment, id } = req.body;
-  const indexedAt = historicalRolls.findIndex((x) => x.id === id);
-  historicalRolls[indexedAt].comment = comment;
+  db.collection("historicalRolls").updateOne(
+    { _id: id },
+    { comment },
+    function (err, res) {
+      if (err) throw err;
+      console.log("1 document updated");
+    }
+  );
   res.json({ comment: comment + " <- your message received!" });
 });
 
